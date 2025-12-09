680fc13db816fd7dc0e29eed01da788034cd41ca
williansilva305-hub <williansilva305@gmail.com>
2025-12-09 00:54:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ADICIONAR ESTE BLOCO PARA RESOLVER O ERRO DE TOP-LEVEL AWAIT
   build: {
     target: 'esnext',
   },
+  // ADICIONAR ESTA LINHA FINAL PARA CORRIGIR O ERRO DE CAMINHO (404)
+  base: './', 
 })
