55fd06e42d451fe86351ff1db42fcf5d78a99fbe
williansilva305-hub <williansilva305@gmail.com>
2025-12-09 01:22:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // FINAL CONFIGURAÇÃO PARA DEPLOY E CORREÇÃO DE ERROS
   base: './', // Fixa o erro de path de assets (Tela Branca/404)
   build: {
     target: 'esnext', // Fixa o erro de Top-level await
