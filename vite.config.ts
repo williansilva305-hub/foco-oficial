30ca9dc6f7a31c47be118948deb7b66832d2d84b
williansilva305-hub <williansilva305@gmail.com>
2025-12-09 00:59:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // FINAL CONFIGURAÇÃO PARA DEPLOY E CORREÇÃO DE ERROS/AVISOS
+  base: './', // Fixa o erro de path de assets (Tela Branca/404)
   build: {
-    target: 'esnext',
+    target: 'esnext', // Fixa o erro de Top-level await
+    chunkSizeWarningLimit: 1000, // Aumenta o limite para remover o aviso de tamanho de arquivo
   },
-  // ADICIONAR ESTA LINHA FINAL PARA CORRIGIR O ERRO DE CAMINHO (404)
-  base: './', 
 })
